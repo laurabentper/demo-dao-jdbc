7c805c2db5df54d51f254126e0c911286fbd50c1
Laura Perlingeiro <laura.bentper@gmail.com>
2024-09-16 13:31:54 -0300

    Department entity class

---
+++ src/application/Program.java
@@ -0,0 +1,12 @@
+package application;
+
+import db.DB;
+import model.Department;
+
+import java.sql.Connection;
+
+public class Program {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ src/db/DbException.java
@@ -0,0 +1,8 @@
+package db;
+
+public class DbException extends RuntimeException {
+    public static final long serialVersionUID = 1L;
+    public DbException(String msg) {
+        super(msg);
+    }
+}

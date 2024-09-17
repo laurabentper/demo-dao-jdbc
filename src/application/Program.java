d8ced61d4aae5982ecf0255e0daecde2aaac84c9
Laura Perlingeiro <laura.bentper@gmail.com>
2024-09-16 23:34:22 -0300

    Insert

--- src/application/Program.java
+++ src/application/Program.java
@@ -29,5 +29,11 @@ public class Program {
         for (Seller s : list) {
             System.out.println(s);
         }
+
+        System.out.println("\n========= TEST 4: seller insert =========");
+        Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
+        sellerDao.insert(newSeller);
+        System.out.println("Inserted! new id = " + newSeller.getId());
+
     }
 }

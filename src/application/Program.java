c05455ae5b165e1c0706dd52064f828b9944a48f
Laura Perlingeiro <laura.bentper@gmail.com>
2024-09-16 22:53:35 -0300

    findById

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,10 +3,17 @@ package application;
 import db.DB;
 import model.Department;
 import model.Seller;
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 
 import java.sql.Connection;
 import java.util.Date;
 
 public class Program {
-
+    public static void main(String[] args) {
+        SellerDao sellerDao = DaoFactory.createSellerDao();
+        System.out.println("========= TEST 1: seller find by ID =========");
+        Seller seller = sellerDao.findById(3);
+        System.out.println(seller);
+    }
 }
